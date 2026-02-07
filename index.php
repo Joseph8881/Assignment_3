8e94ae8441627f53c0169361feb29fb28b69e763
Joseph8881 <fredpoku88@gmail.com>
2026-02-06 21:27:06 -0400

    index code

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment 3 - Input</title>
+</head>
+<body>
+    <h2>Assignment #3</h2>
+
+    <form method="post" action="result.php">
+        X: <input type="number" name="x" required><br><br>
+        Y: <input type="number" name="y" required><br><br>
+        Z: <input type="number" name="z" required><br><br>
+
+        <input type="submit" value="Calculate">
+    </form>
+</body>
+</html>
